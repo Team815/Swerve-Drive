80ff8d827dd1f7431ba1ca202929edca3df0514f
Brian Wirsing <beamer159@gmail.com>
2022-12-15 14:53:55 -0500

    Setup start code

--- core/src/frc/team815/SwerveProgram.java
+++ core/src/frc/team815/SwerveProgram.java
@@ -9,29 +9,20 @@ public class SwerveProgram extends ApplicationAdapter {
 	final int axisLeftX = 0;
 	final int axisLeftY = 1;
 	final int axisRightX = 2;
-	final int axisRightY = 3;
 
 	Controller controller;
-	SwerveDrive swerveDrive;
 
 	
 	@Override
 	public void create () {
 		controller = Controllers.getCurrent();
-		swerveDrive = new SwerveDrive(.76, .76);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(.8f, .8f, .8f, 1);
-		var leftY = controller.getAxis(axisLeftY);
 		var leftX = controller.getAxis(axisLeftX);
+		var leftY = controller.getAxis(axisLeftY);
 		var rightX = controller.getAxis(axisRightX);
-		swerveDrive.setModules(leftX, -leftY, rightX);
-		swerveDrive.render();
-	}
-
-	@Override
-	public void dispose () {
 	}
 }
